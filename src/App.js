535d53360fedbda1ae4fc332cc73ede713103336
Mihail Bezev <45494812+mivebe@users.noreply.github.com>
2020-10-04 20:39:15 +0300

    added responsive sticky navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import logo from "./media/logo.jpg"
+
+import "./styles/App.css";
+import "./styles/Navbar.css"
+import "./styles/Footer.css"
+
+const App = () => (
+    <div id="body">
+        <nav>
+            <input id="nav-toggle" type="checkbox"></input>
+            <div className="logo"><img id="logo" src={logo} alt="404" height="60vh" /><strong>M</strong>i<strong>V</strong>e<strong>B</strong>e</div>
+            <ul className="links">
+                <li><a href="#home">Trending</a></li>
+                <li><a href="#home">Favorites</a></li>
+                <li><a href="#home">Uploaded</a></li>
+                <li><input type="text" placeholder="Search for a Gif"></input></li>
+                <li><a href="#home">Search</a></li>
+            </ul>
+            <label htmlFor="nav-toggle" className="icon-burger">
+                <div className="line"></div>
+                <div className="line"></div>
+                <div className="line"></div>
+            </label>
+        </nav>
+        <div id="container">
+            <img id="logo" src={logo} alt="404" />
+            <img id="logo" src={logo} alt="404" />
+        </div>
+        <footer>© mivebe</footer>
     </div>
-  );
-}
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+//import $ from "jquery"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import App from "./App.js";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById("root"));
+
+/*
+$("#nav").on("change", () => {
+    if ($('#navInnerWrapper').height() > $('#nav').height()) {
+        $('#nav').height($('#navInnerWrapper').height());
+    }
+});*/
